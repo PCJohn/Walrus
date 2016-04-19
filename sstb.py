0a19e721aa1ad6fc0bcdf856dc81fe8ab1ccb827
Prithvijit Chakrabarty <prithvichakra@gmail.com>
2016-04-19 21:22:36 +0530

    Added files via upload
    We are all together...

--- sstb.py
+++ sstb.py
@@ -1,13 +1,19 @@
+from __future__ import division
 import os
+import pickle
 import numpy as np
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
 from sknn.mlp import Convolution, Layer, Classifier, Regressor
+from sknn.platform import cpu32, threading
+#import logging
+
+#logging.basicConfig()
 
 phrase_path = './Data/SSTb/dictionary.txt'
 glove_path = './Data/glove.6B/glove.6B.50d.txt'
 sent_path = './Data/SSTb/sentiment_labels.txt'
-model_path = './SSTb_model'
+model_path = './SSTb_model_fast_train.pkl'
 
 lm = WordNetLemmatizer()
 
@@ -83,24 +89,72 @@ def load_ds():
 def train(X,Y):
 	print X.shape
 	print Y.shape
+	trainX = X[:int(X.shape[0]*0.7),:,:]
+	trainY = Y[:int(Y.shape[0]*0.7),:]
+	valX = X[int(X.shape[0]*0.7):int(X.shape[0]*0.8),:,:]
+	valY = Y[int(Y.shape[0]*0.7):int(Y.shape[0]*0.8),:]
+	testX = X[int(X.shape[0]*0.8):,:,:]
+	testY = Y[int(Y.shape[0]*0.8):,:]
+
+	print 'Train, Val, Test'
+	print trainX.shape,',',trainY.shape,'--',valX.shape,',',valY.shape,'--',testX.shape,',',testY.shape
+
 	nn = Classifier(
 		layers = [
 			Convolution('Rectifier',channels=1,kernel_shape=(5,WORD_DIM)),
 			Layer('Rectifier',units=300),
 			Layer('Rectifier',units=300),
 			Layer('Softmax')],
-		learning_rate = 0.001,
+		#valid_set = (valX,valY),
+		learning_rate = 0.02, #0.05, #0.001,
+		#normalize='batch',
 		verbose = True)
 	print 'Net created...'
-	#nn.n_iter = 150
-	nn.fit(X,Y)
-	pickle.dump(nn,open(model_path,'w'))
-	print 'Model saved'
+	#Load net here --always CHECK HERE before starting -- DO THIS NOW, WE WANT TO CONTINUE FROM HERE ON
+	nn = pickle.load(open(model_path,'rb'))
+	for i in range(100):
+		try:
+			nn.n_iter = 5
+			nn.fit(trainX,trainY)
+			pickle.dump(nn,open(model_path,'wb'))
+			nn = pickle.load(open(model_path,'rb'))
+		except KeyboardInterrupt:
+			pickle.dump(nn,open(model_path,'wb'))
+			print 'Saved model after keyboard interrupt'
+		pickle.dump(nn,open(model_path,'wb'))
+		print 'Temp model saved'
+
+	#try:
+	#	nn.fit(trainX,trainY)
+	#except KeyboardInterrupt:
+	#	pickle.dump(nn,open(model_path,'wb'))
+
+	print 'Done, final model saved'
+
+def test_batch(nn,testX,testY):
+	Y = nn.predict(testX)
+	correct = 0
+	total = 0
+	print Y.shape,'==',testY.shape
+	for i,y in enumerate(Y):
+		if (y == testY).all():
+			correct = correct+1
+		total = total+1
+	print (correct/total)
 
 def test(path):
-	pass
+	nn = pickle.load(open(model_path,'rb'))
+	glove = load_glove()
+	while True:
+		line = raw_input('Enter text: ')
+		if len(line) == 0:
+			continue
+		#Find sentiment here
+		em = encode_phrase(glove,line)
+		print em.shape
+		print nn.predict(np.array([em]))
 
 x,y = load_ds()
 print x.shape,'==',y.shape
 train(x,y)
-test(model_path)
+#test(model_path)
